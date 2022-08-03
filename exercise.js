6a3b00f72c42bdfa353d12e7012ff8b3993fe6b3
olaolujnr <shololaolaoluwa@gmail.com>
2022-08-03 11:24:24 +0100

    day 1 exercises

---
+++ exercise.js
@@ -0,0 +1,49 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavasscript
+/*comments can make code readable
+easy to use and informative*/
+
+
+//exercise 6; day 1 Declare four variables without assigning values
+let score
+let location
+let bill
+let isCold
+
+//exercise 7; day1 Declare four variables with assigned values
+let a = 7
+let b = "bola"
+let isHot = true
+let phoneNumber = null
+
+/*exercise 8; day 1
+Declare variables to store your first name, last name, 
+marital status, country and age in multiple lines
+*/
+let firstName = "Olaoluwa"
+let lastName = "Sholola"
+let isMarried = false 
+let country = "Nigeria"
+let age = 20
+
+/*exercise 9; day 1
+Declare variables to store your first name, last name,
+marital status, country and age in a single line*/
+
+let firstName = "Olaoluwa"; lastName = "Sholola"; isMarried = false ; country = "Nigeria"; age = 20;
+
+/*exeercise 10; day 1
+Declare two variables myAge and yourAge and assign them
+initial values and log to the browser console */
+
+let myAge = 20
+let yourAge = 30
+
+let myAgeDesc = `i am ${myAge} years old`
+let yourAgeDesc = `you are ${yourAge} years old`
+
+console.log(myAgeDesc)
+console.log(yourAgeDesc)
+
+console.log( )
+ 
